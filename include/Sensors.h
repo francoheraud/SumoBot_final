6728e7e2c9c28ab74aaafdbc0feb9185b55a4e83
Allan Wu <allan.wu@outlook.com.au>
2025-10-16 01:33:58 +0800

    changed out of range to 999

--- include/Sensors.h
+++ include/Sensors.h
@@ -13,7 +13,7 @@
 
 #define LEFT_ULTRASONIC 	0
 #define RIGHT_ULTRASONIC 	1
-#define OUT_OF_RANGE 		-1
+#define OUT_OF_RANGE 		999
 
 // Set the maximum time we will wait for the echo pulse: this determines what is "OUT OF RANGE" for the sensor!
 // From my testing, 15000 us timeout limits the distance range to ~200 cm
@@ -29,7 +29,7 @@ extern int ADCLookupDefaults[16];
 extern const char *ADCStrings[16];
 
 typedef struct {
-	// Ultrasonic sensor distances stored to the nearest cm, -1 is OUT_OF_RANGE
+	// Ultrasonic sensor distances stored to the nearest cm, 999 is OUT_OF_RANGE
 	int leftCm;
 	int rightCm;
 
